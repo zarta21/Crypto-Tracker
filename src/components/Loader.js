d3c88a92fb61b734d42abc31c7c872264bda1a92
Tautvydas <95254346+zarta21@users.noreply.github.com>
2024-01-27 22:03:58 +0100

    Add files via upload

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+        <div className="wrapper">
+            <span></span>
+        </div>
+    </div>
+  )
+}
+
+export default Loader
